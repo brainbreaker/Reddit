5486fef9330385e4b9f7adddc3246e0e6550727a
Gautam Prajapati <gautamprajapati06@gmail.com>
2018-05-18 19:38:42 +0530

    Update the DB URI to have retryWrites false

--- Reddit-Server/config/db.js
+++ Reddit-Server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // MongoDB database URI
-const uri = "mongodb://carousell:carousell@greddit-shard-00-00-njkug.mongodb.net:27017,greddit-shard-00-01-njkug.mongodb.net:27017,greddit-shard-00-02-njkug.mongodb.net:27017/test?ssl=true&replicaSet=greddit-shard-0&authSource=admin&retryWrites=true";
+const uri = "mongodb://carousell:carousell@greddit-shard-00-00-njkug.mongodb.net:27017,greddit-shard-00-01-njkug.mongodb.net:27017,greddit-shard-00-02-njkug.mongodb.net:27017/test?ssl=true&replicaSet=greddit-shard-0&authSource=admin&retryWrites=false";
 // Mongoose Options
 const options = {
   reconnectTries: Number.MAX_VALUE,
