331bf2be452cb2ab2b7bad8e82a1db976db1e5cd
Gautam Prajapati <gautamprajapati06@gmail.com>
2018-05-18 19:39:34 +0530

    Write test for creating a new post and rename the root.js to allPosts.js

---
+++ Reddit-Server/test/createPost.js
@@ -0,0 +1,30 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
+const Post = require('../models/post');
+var url = "http://localhost:" + (process.env.PORT || 8000);
+
+
+describe('Create a new post', () => {
+  it('Should create a new sample reddit post at POST /posts', (done) => {
+    	
+	  chai.request(url)
+	    .post('/posts')
+	    .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ title: "Reddit post title", image_url: "https://www.example.com", description: "post description" })
+	    .end(function (err, res){
+	    	if (err) {
+	          done(err)
+	        }
+	        res.status.should.be.equal(200)
+
+			// Now delete the newly created post	        
+	        chai.request(url)
+	        	.delete('/posts/' + res.body._id)
+	        	.end(function(err, res){
+		          res.should.have.status(200);
+		          done()
+			    });
+	    });
+  })
+})
