966008ee6a66755814fe24180dc596325613de76
Gautam Prajapati <gautamprajapati06@gmail.com>
2018-05-20 13:00:19 +0530

    Add code for fetching the list of posts

--- Reddit-Android/app/src/main/java/com/carousell/brainbreaker/reddit/MainActivity.java
+++ Reddit-Android/app/src/main/java/com/carousell/brainbreaker/reddit/MainActivity.java
@@ -1,22 +1,48 @@
 package com.carousell.brainbreaker.reddit;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.carousell.brainbreaker.reddit.Adapter.RecyclerViewAdapter;
+import com.carousell.brainbreaker.reddit.Model.Post;
+import com.carousell.brainbreaker.reddit.Retrofit.RedditAPIInterface;
+import com.carousell.brainbreaker.reddit.Retrofit.RetrofitAPIClient;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RedditAPIInterface redditAPIInterface;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    RecyclerView postsRecyclerView;
+    ArrayList<Post> postArrayList;
+    SweetAlertDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        postsRecyclerView = (RecyclerView) findViewById(R.id.postsRecyclerView);
+        postsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +52,77 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        postArrayList = new ArrayList<>();
+        redditAPIInterface = RetrofitAPIClient.getClient().create(RedditAPIInterface.class);
+
+        if (isNetworkAvailable(this)) {
+            fetchAllPosts();
+            showProgressDialog();
+        }
+    }
+
+    /**
+     * Fetch the top 20 posts sorted by votes from our API
+     * and populate the recycler view
+     */
+    private void fetchAllPosts() {
+        Call<ArrayList<Post>> call = redditAPIInterface.getAllPosts();
+        call.enqueue(new Callback<ArrayList<Post>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Post>> call, Response<ArrayList<Post>> response) {
+                postArrayList = response.body();
+                dismissProgressDialog();
+                recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, postArrayList);
+                postsRecyclerView.setAdapter(recyclerViewAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Post>> call, Throwable t) {
+                showErrorDialog();
+            }
+        });
+    }
+
+
+    /**
+     * A function to check whether we're connected to internet or not
+     */
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    /**
+     * Shows a sweetalert progress dialog
+     */
+    public void showProgressDialog() {
+        pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText(getString(R.string.loading_Text));
+        pDialog.setCancelable(false);
+        pDialog.show();
+    }
+
+    /**
+     * Dismiss the sweetalert progress dialog
+     */
+    public void dismissProgressDialog() {
+        if(pDialog.isShowing()) {
+            pDialog.dismissWithAnimation();
+        }
+    }
+
+    /**
+     * Shows a sweetalert error dialog
+     */
+    public void showErrorDialog() {
+        new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                .setTitleText("Oops...")
+                .setContentText("Something went wrong!")
+                .show();
     }
 
     @Override
